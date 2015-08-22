65783458e98052fe54bd233c59b17b4f3f7647f4
yfuka86 <yfuka86@gmail.com>
2015-08-22 13:36:10 +0900

    fix integration establish

--- app/controllers/integrations_controller.rb
+++ app/controllers/integrations_controller.rb
@@ -4,13 +4,12 @@ class IntegrationsController < WebBaseController
   def establish
     return if !params[:provider] || !params[:team_id] || !current_user
     klass = "Integration::#{params[:provider].classify}".constantize
-    team = current_user.teams.find_by(external_cid: params[:team_id])
-    klass.establish(team, current_user, params[:options])
+    klass.establish(current_user, params[:options])
     redirect_to "/auth/#{params[:provider]}"
   end
 
   def incoming_webhook
-    webhook = IntegrationWebhook.find_by(uid: params[:webhook_uid]git a
+    webhook = IntegrationWebhook.find_by(uid: params[:webhook_uid])
     if webhook.try(:execute, JSON.parse(params[:payload]))
       render json: { status: :ok }
     else
