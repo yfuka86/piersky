6e9e6374ac32bfe7a41ff880da32d0ec32b71ab4
garasubo <garasubo@gmail.com>
2015-08-22 17:41:48 +0900

    remove comment

--- app/controllers/api/slack_wrapper_controller.rb
+++ app/controllers/api/slack_wrapper_controller.rb
@@ -3,13 +3,11 @@ module Api
     before_action :set_slack
 
     def index
-      @slack = Integration.find_by(user: current_user, type: Integration::Slack)
       channels = @slack.show_channels
       render json: { success: true, channels: channels }.to_json
     end
 
     def show
-      @slack = Integration.find_by(user: current_user, type: Integration::Slack)
       messages = @slack.show_messages(params[:id], params[:ts])
       render json: { success: true, messages: messages }.to_json
     end
@@ -17,8 +15,6 @@ module Api
     private
     def set_slack
       @slack = Integration.find_by(user: current_user, type: Integration::Slack)
-      Rails.logger.debug "java"
-      Rails.logger.debug @slack.token
     end
 
   end
