d9f767f4427e0f04be0e0195582834a1d5ab968c
garasubo <garasubo@gmail.com>
2015-08-23 15:10:20 +0900

    fix

--- app/assets/javascripts/src/components/statistics/slack.js
+++ app/assets/javascripts/src/components/statistics/slack.js
@@ -130,11 +130,6 @@ class Slack extends React.Component {
         })}
         </ul>
         </div>
-        <BarChart
-            data={data}
-            width={400}
-            height={400}
-            margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
       </div>
     );
   }
