97acae870fdb9187efcd7b74e284e54eef60089c
yfuka86 <yfuka86@gmail.com>
2015-09-12 10:54:16 +0900

    fix redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,2 @@
-require 'connection_pool'
-
 REDIS_HOST = ENV['REDIS_HOST'] || '127.0.0.1'
-Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(host: REDIS_HOST, port: 6379) }
+Redis.current = Redis.new(host: REDIS_HOST, port: 6379)
