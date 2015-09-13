ce518006d97c0878e14d2702425fd8e7e21ac987
yfuka86 <yfuka86@gmail.com>
2015-09-14 03:12:46 +0900

    don't serve 500 404page in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  if !Rails.env.development?
-    rescue_from Exception,                        with: :render_500
-    rescue_from ActiveRecord::RecordNotFound,     with: :render_404
-    rescue_from ActionController::RoutingError,   with: :render_404
-  end
+  # if !Rails.env.development?
+  #   rescue_from Exception,                        with: :render_500
+  #   rescue_from ActiveRecord::RecordNotFound,     with: :render_404
+  #   rescue_from ActionController::RoutingError,   with: :render_404
+  # end
 
   def routing_error
     raise ActionController::RoutingError.new(params[:path])
