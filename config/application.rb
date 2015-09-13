eaca59c81a28f25560eaa6de08e110c61a8e55e2
yfuka86 <yfuka86@gmail.com>
2015-09-13 16:19:56 +0900

    delete config of initialize_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,6 @@ module Piersky
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.initialize_on_precompile = false
-
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.paths << Rails.root.join('node_modules', 'react-tooltip', 'dist')
 
