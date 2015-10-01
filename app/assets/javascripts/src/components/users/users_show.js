5718d0653e313fe76136b97f144fa0c7f3e313a1
garasubo <garasubo@gmail.com>
2015-10-01 23:18:59 +0900

    fix user board

--- app/assets/javascripts/src/components/users/users_show.js
+++ app/assets/javascripts/src/components/users/users_show.js
@@ -18,9 +18,70 @@ class UsersShow extends React.Component {
 
   getParamsFromStores(props) {
     let user = UserStore.getUserById(props.params.id)
+    let date = [
+      '2015/08/07',
+      '2015/08/08',
+      '2015/08/09',
+      '2015/08/10',
+      '2015/08/11',
+      '2015/08/12',
+      '2015/08/13',
+      '2015/08/14',
+      '2015/08/15',
+      '2015/08/16',
+      '2015/08/17',
+      '2015/08/18',
+      '2015/08/19',
+      '2015/08/20',
+      '2015/08/21',
+      '2015/08/22',
+      '2015/08/23'
+    ];
+    let data=[ { type: "Slack", data:[
+      1000,
+      1170,
+      1660,
+      1030,
+      1200,
+      117,
+      360,
+      1030,
+      900,
+      1170,
+      660,
+      1030,
+      200,
+      370,
+      660,
+      1000,
+      1170
+    ]},
+    { type: "Github", data:[
+      400,
+      460,
+      1120,
+      540,
+      400,
+      260,
+      100,
+      540,
+      400,
+      460,
+      1120,
+      540,
+      100,
+      60,
+      1120,
+      400,
+      460
+    ]}
+    ];
+
     return {
       user: user,
-      identities: IdentityStore.getIdentitiesByUserId(user.id)
+      identities: IdentityStore.getIdentitiesByUserId(user.id),
+      date: date,
+      data: data
     };
   }
 
@@ -35,27 +96,23 @@ class UsersShow extends React.Component {
   componentWillUnmount() {
   }
 
+  onChange(e) {
+    this.setState(this.getParamsFromStores(this.props));
+    this.drawChart();
+  }
+
   drawChart() {
-    var data = google.visualization.arrayToDataTable([
-      ['date', 'Slack', 'Github'],
-      ['2015/08/07',  1000,      400],
-      ['2015/08/08',  1170,      460],
-      ['2015/08/09',  1660,       1120],
-      ['2015/08/10',  1030,      540],
-      ['2015/08/11',  1200,      400],
-      ['2015/08/12',  117,      260],
-      ['2015/08/13',  360,       100],
-      ['2015/08/14',  1030,      540],
-      ['2015/08/15',  900,      400],
-      ['2015/08/16',  1170,      460],
-      ['2015/08/17',  660,       1120],
-      ['2015/08/18',  1030,      540],
-      ['2015/08/19',  200,      100],
-      ['2015/08/20',  370,      60],
-      ['2015/08/21',  660,       1120],
-      ['2015/08/22',  1000,      400],
-      ['2015/08/23',  1170,      460]
-    ]);
+    var column =['date'];
+    var values = this.state.date;
+    this.state.data.forEach((item, idx) => {
+      column = column.concat([item.type]);
+      item.data.forEach((val,jdx) => {
+        if(idx==0) values[jdx] = [values[jdx],val];
+        else values[jdx] = values[jdx].concat([val]);
+      });
+    });
+    var arr = [column].concat(values);
+    var data = google.visualization.arrayToDataTable(arr);
 
     var options = {
       title: 'activity',
