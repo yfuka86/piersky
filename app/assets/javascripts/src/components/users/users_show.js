18108579e3510449fd0dcfea185e0e3ae6cde8b4
garasubo <garasubo@gmail.com>
2015-10-05 22:32:58 +0900

    use real data

--- app/assets/javascripts/src/components/users/users_show.js
+++ app/assets/javascripts/src/components/users/users_show.js
@@ -24,63 +24,21 @@ class UsersShow extends React.Component {
 
   getParamsFromStores(props) {
     let user = UserStore.getUserById(props.params.id)
-    let data= {};
-    data["Slack"]=[
-      1000,
-      1170,
-      1660,
-      1030,
-      1200,
-      117,
-      360,
-      1030,
-      900,
-      1170,
-      660,
-      1030,
-      200,
-      370,
-      660,
-      1000,
-      1170
-    ];
-    data["Github"] = [
-      400,
-      460,
-      1120,
-      540,
-      400,
-      260,
-      100,
-      540,
-      400,
-      460,
-      1120,
-      540,
-      100,
-      60,
-      1120,
-      400,
-      460
-    ];
 
     return {
       user: user,
       identities: IdentityStore.getIdentitiesByUserId(user.id),
       integrations: IntegrationStore.getIntegrations(),
       statistics: StatisticsStore.getStats(),
-      data: data
     };
   }
 
   componentDidMount() {
 
-
     IntegrationAction.load().then(() => {
       let p = IntegrationStore.getIntegrations().filter((integ) => {
         return StatisticsStore.getStatById(integ.id)==null;
       }).map((integ) => {
-        console.log("send stat");
         return IntegrationAction.stat(integ.id);
       })
       return Promise.all(p);
@@ -106,16 +64,24 @@ class UsersShow extends React.Component {
     let data = this.state.data;
     console.log(this.state);
 
+    let identities = this.state.identities;
     let integrations = _.groupBy(this.state.integrations, (integration) => {
       return integration.type;
     });
+    let statistics = this.state.statistics;
 
     var table =[['Date'].concat(_.map(integrations,(_,key) => key))];
-    let length = 10;
+    let length = 31;
     let end = new Date(2014, 8, 25);
     _.times(length, (i) =>{
       let ary = _.map(integrations,(group, key) => {
-        return data[key][length - (i+1)];
+        let identity = _.find(identities,{ 'type': key })
+        return _.reduce(group, (total, integration) => {
+          let stat = statistics[integration.id];
+          let tar = _.find(stat.identities, { 'id': identity.id });
+          if(tar) return total + tar.default[length - (i + 1)];
+          return total;
+        }, 0);
       });
       table.push([moment().subtract(length - (i + 1), 'days').format("YYYY/MM/DD")].concat(ary));
     });
