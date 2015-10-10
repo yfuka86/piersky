ddaff1f9a7b9fbe11cda3097fb9893e6ec70f528
yfuka86 <yfuka86@gmail.com>
2015-10-10 09:10:41 +0900

    hotfix

--- app/serializers/api/statistics/user_serializer.rb
+++ app/serializers/api/statistics/user_serializer.rb
@@ -12,19 +12,20 @@ class Api::Statistics::UserSerializer < ActiveModel::Serializer
   def integrations
     period = SkyModule.get_period
     user_team = object.user_teams.find_by(team: object.current_team)
-    identity_ids = user_team.identities.pluck(:integration_id).uniq
-    integrations = ::Integration.where(id: identity_ids)
+    identities =  user_team.identities
+    identity_ids = identities.pluck(:id)
+    integration_ids = identities.pluck(:integration_id).uniq
+    integrations = ::Integration.where(id: integration_ids)
 
     integrations.map do |integration|
       q = integration.activity_class.where(identity_id: identity_ids)
-      [q.count, period]
-      # daily_counts = q.where(ts: period).group("date_trunc('day', ts)").count
-      # hourly_counts = q.group("date_part('hour', ts)").count
-      # {
-      #   id: integration.id,
-      #   default: period.map{|d| daily_counts.select{|k, v| k == d}.values.sum }.reverse,
-      #   day: (0..23).map{|h| hourly_counts.select{|k, v| k == h}.values.sum }
-      # }
+      daily_counts = q.where(ts: period).group("date_trunc('day', ts)").count
+      hourly_counts = q.group("date_part('hour', ts)").count
+      {
+        id: integration.id,
+        default: period.map{|d| daily_counts.select{|k, v| k == d}.values.sum }.reverse,
+        day: (0..23).map{|h| hourly_counts.select{|k, v| k == h}.values.sum }
+      }
     end
   end
 end
