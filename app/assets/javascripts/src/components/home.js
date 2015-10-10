91e861eb8f4fd74046b7e8c9ce01192db9dd40b8
yfuka86 <yfuka86@gmail.com>
2015-10-10 11:02:10 +0900

    temp

--- app/assets/javascripts/src/components/home.js
+++ app/assets/javascripts/src/components/home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Link, RouteHandler} from 'react-router';
 import _ from 'lodash';
 
+import UserStore from '../stores/user';
+import UserIcon from '../components/users/user_icon';
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -10,30 +13,65 @@ class Home extends React.Component {
   }
 
   get initialState() {
-    return _.extend({
-    });
+    return {
+      users: UserStore.getUsers()
+    };
   }
 
   componentDidMount() {
+    UserStore.onChange(this.onChangeHandler);
   }
 
   componentWillUnmount() {
+    UserStore.offChange(this.onChangeHandler);
   }
 
   onChange(e) {
-    this.setState(this.initialState);
+    this.setState(this.InitialState);
   }
 
   render() {
     return (
       <div className='home container'>
+        <p className='title'>{I18n.t('webapp.home.index')}</p>
         <div className='team-summary'>
-          <p className='title'>{I18n.t('webapp.users.index')}</p>
-          <div className='users-index'>
-
+          <div className='option-header'>
+            <div className='content-area'>
+              <div className='name' />
+              <span className='right-content'>
+                <p className='main-content activity'>{I18n.t('integration.general.activities')}<br/>{I18n.t('integration.index.last_31_days')}</p>
+                <div className='user-graph' />
+                <div className='link' />
+              </span>
+            </div>
           </div>
+
+          {this.state.users.map((user) =>{
+            let summary = user.summary
+            return (
+              <div className='option' key={user.id}>
+                <div className='content-area'>
+                  <div className='icon-area'>
+                    <UserIcon user={user} />
+                  </div>
+                  <Link to='user-show' params={{id: user.id}} className='link'>
+                    <p className='name'>
+                      {user.identity()}
+                    </p>
+                  </Link>
+
+                  <span className='right-content'>
+                    <p className='main-content activity'>{_.sum(user.summary)}</p>
+                    <div className='user-graph' id={`user_graph_${user.id}`} />
+                    <Link to='user-show' params={{id: user.id}} className='link'>
+                      <button>{I18n.t('user.index.view_detail')}</button>
+                    </Link>
+                  </span>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <div className='clear-fix' />
       </div>
     );
   }
