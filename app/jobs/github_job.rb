a568b41d6bcf2c547ef613c949e9a19c0a343c15
yfuka86 <yfuka86@gmail.com>
2015-11-08 14:55:43 +0900

    bundle update

---
+++ app/jobs/github_job.rb
@@ -0,0 +1,31 @@
+class GithubJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(integration_id)
+    # ActiveRecord::Base.transaction do
+    #   integration = IntegrationGithub.find_by(id: integration_id)
+    #   webhooks = integration.webhooks.pluck(:name)
+
+    #   team = SlackTeam.find_or_create(integration.team_info, integration)
+
+    #   # fix range of time
+    #   now = DateTime.now.to_f
+    #   latest_persisted = (ActivitySlack.latest_ts(integration) || 1.0).to_f
+    #   #
+
+    #   integration.show_channels.each do |c|
+    #     channel = SlackChannel.find_or_create(c, integration)
+
+    #     oldest_in_fetching = now
+    #     until (messages = integration.show_messages(channel, oldest: latest_persisted, latest: oldest_in_fetching)) && messages.length == 0
+    #       messages.each do |m|
+    #         ActivitySlack.create_with_integration(m, channel, integration)
+    #       end
+    #       oldest_in_fetching = messages.last["ts"].to_f
+    #     end
+    #   end
+
+    #   integration.default!
+    # end
+  end
+end
