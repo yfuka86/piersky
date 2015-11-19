b3e8517f7b1f39e762cd1f4316c81c311192c102
Yuta Fukazawa <yuta@piersky.com>
2015-11-19 14:24:58 +0900

    Update mail_preview.rb

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,6 +1,6 @@
 class MailPreview < MailView
-	def daily_mail
-		user_team_id = 1
-		mail = SummaryMailer.daily_mail(user_team_id)
-	end
+  def daily_mail
+    user_team_id = 1
+    mail = SummaryMailer.daily_mail(user_team_id)
+  end
 end
