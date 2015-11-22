3b472380397c29a8dd537323fd836f1b30ffa615
yfuka86 <yfuka86@gmail.com>
2015-11-22 20:39:37 +0900

    put user iterator

--- app/presenters/daily_summary_presenter.rb
+++ app/presenters/daily_summary_presenter.rb
@@ -1,13 +1,16 @@
 class DailySummaryPresenter
 
-  attr_accessor :text, :graph
+  attr_accessor :graph, :text, :users
 
   def initialize(user_team_id)
     user_team = UserTeam.find(user_team_id)
     user = user_team.user
     team = user_team.team
-    @text = team.integrations.map(&:daily_summary)
     @graph = team_daily_graph(team)
+    summary = team.integrations.map(&:daily_summary)
+    @users = team.users.map do |u|
+      u
+    end
   end
 
   def team_daily_graph(team)
