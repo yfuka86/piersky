a69275230b1e41eb74f496df835d4c162b54816e
yfuka86 <yfuka86@gmail.com>
2015-12-14 08:42:24 +0900

    fix via controller authorization

--- app/controllers/via_controller.rb
+++ app/controllers/via_controller.rb
@@ -1,6 +1,8 @@
 class ViaController < WebBaseController
+  skip_before_action :authenticate_user!
+
   def mail
-    redirect_to params[:url] || root_path
+    redirect_to(params[:url] || root_path)
     Log.create(code: :mail, body: params[:url])
   end
 end
