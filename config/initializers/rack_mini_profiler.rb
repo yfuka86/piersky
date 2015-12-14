0643a91b3f026ef973acdd604532f201d778ce3f
yfuka86 <yfuka86@gmail.com>
2015-12-14 13:56:08 +0900

    fix

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -1,6 +1,5 @@
 if Rails.env.development?
   require 'rack-mini-profiler'
-  alt + P で表示/非表示の切り替え
   Rack::MiniProfilerRails.initialize!(Rails.application)
   Rack::MiniProfiler.config.position = 'right'
   Rack::MiniProfiler.config.start_hidden = true
